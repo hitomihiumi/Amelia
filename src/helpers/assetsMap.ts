0c2e78bf79c61148972cfc6db6fecc0ff256728f
hitomihiumi <amdroidbox19@gmail.com>
2025-10-28 18:24:13 +0200

    feat: add SVG assets and font mapping to assetsMap

--- src/helpers/assetsMap.ts
+++ src/helpers/assetsMap.ts
@@ -1,5 +1,13 @@
 import path from "node:path";
+import { Font, FontWeight } from "@nmmty/lazycanvas";
 
 export const assetsMap: Record<string, string> = {
-  microphone: path.resolve(__dirname, "../../assets/microphone.png"),
+  microphone: path.resolve(__dirname, "../../assets/img/mic.png"),
+};
+
+export const fontMap: Record<string, Font> = {
+  wdxllubrifont: new Font()
+    .setFamily("WDXL Lubrifont")
+    .setWeight(FontWeight.Regular)
+    .setPath(path.resolve(__dirname, "../../assets/fonts/WDXLLubrifontSC.ttf")),
 };
