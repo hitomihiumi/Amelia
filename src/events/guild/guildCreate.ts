b018a5ce83a9d7e2cc4d8e43fb61b433eec481d5
hitomihiumi <amdroidbox19@gmail.com>
2026-04-14 00:24:29 +0300

    feat: instantiate GuildClass in guildCreate handler

--- src/events/guild/guildCreate.ts
+++ src/events/guild/guildCreate.ts
@@ -1,3 +1,6 @@
 import { Client, Guild } from "discord.js";
+import { Guild as GuildClass } from "../../helpers/Guild";
 
-module.exports = async (client: Client, guild: Guild) => {};
+module.exports = async (client: Client, guild: Guild) => {
+    new GuildClass(client, guild);
+};
